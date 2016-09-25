86615a5987ff1a1c2662220cb454868d17442191
Piero Giusti <pierophp@gmail.com>
2016-09-24 22:38:43 -0300

    Fix i18n

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -5,6 +5,8 @@
       <div id="content">
         <p>Language: {{$language}}</p>
         <p>{{$i18n.message.hello}}, {{$i18n.message.world}}</p>
+        <a @click="$setLanguage('en-US')">English</a>
+        <a @click="$setLanguage('pt-BR')">Portguese</a>
         <router-view transition="fade" transition-mode="out-in" class="animated"></router-view>
       </div>
     </div>
@@ -16,6 +18,8 @@ import MainMenu from './components/menu/MainMenu';
 export default {
   components: {
     MainMenu,
+  },
+  methods: {
   },
 };
 </script>
--- ui/src/i18n.js
+++ ui/src/i18n.js
@@ -1,33 +1,59 @@
 /* eslint no-param-reassign: ["error", { "props": false }]*/
+/* eslint no-underscore-dangle: "off"*/
+import jquery from 'jquery';
+
+/**
+ * Updates all the watchers in the Vue instance of a component tree.
+ *
+ * This function is inspired by the "_digest()" function in the
+ * "src/instance/scope.js" of the source of Vue.js, excepts that this function
+ * updates the children components no matter whether it is inheritable.
+ *
+ * @param vm
+ *    the root of the component tree.
+ */
+function update(vm) {
+  if (!vm._watchers) {
+    return;
+  }
+
+  let i = vm._watchers.length;
+  while (i--) {
+    vm._watchers[i].update(true); // shallow updates
+  }
+  const children = vm.$children;
+  i = children.length;
+  while (i--) {
+    const child = children[i];
+    update(child);
+  }
+}
+
+
 const I18n = {
   install(Vue) {
-    const jquery = window.JQuery || window.$;
-    if (!jquery) {
-      throw new Error('JQuery is required.');
-    }
-
     Vue.prototype.$language = 'pt-BR';
     Vue.prototype.$i18n = {};
     Vue.prototype.$setLanguage = function setLanguage(language, callback) {
+      const vm = this;
       Vue.prototype.$language = '';
       Vue.prototype.$i18n = {};
-
       jquery.ajax({
-        url: `assets/i18n/${language}.json`,
+        url: `static/i18n/${language}.json`,
+        async: false,
         dataType: 'json',
         type: 'GET',
         success(data) {
           Vue.prototype.$language = language;
           Vue.prototype.$i18n = data;
-          // update(vm.$root);
+
+          console.log(vm.$root);
+          update(vm.$root);
           if (callback) {
             callback();
           }
         },
       });
-      // Vue.$http.get(, (res) => {
-      //  console.log(res);
-      // });
     };
   },
 };
